426fb51bcc8374b64673d3c9313aedfd295bd671
cybermuqaddas <m2818989@gmail.com>
2026-02-23 07:04:11 +0500

    11-darsning1-qismi

---
+++ temp.py
@@ -0,0 +1,20 @@
+mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+savat = []
+for n in range(5):
+    savat.append(input(f'Savatga {n+1}-mahsulotni qoshing: '))
+
+bor_mahsulotlar = []
+mavjud_emas = []
+for mahsulot in savat:
+    if mahsulot in mahsulotlar:
+        bor_mahsulotlar.append(mahsulot)
+    else:
+        mavjud_emas.append(mahsulot)
+
+if mavjud_emas:
+  print("Do'konimizda quyidagi mahsulotlar yo'q:")
+for mahsulot in mavjud_emas:
+  print(mahsulot)
+else:
+  print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
